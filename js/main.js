9564350328a9d8ddae4530965534a00eec0fee1a
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-02 16:01:23 +0100

    creata scelta difficolta gioco

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
-
+let choiseDifficult = prompt("Benvenuto al gioco Campo minato: scegli difficolta: 1, 2, 3");
+const difficolta = ["1", "2", "3"];
 const gridCont = document.querySelector(".grid");
 
-for (i = 0; i < 100; i++) {
-    let newElement = generaElemento("div", "square");
-    gridCont.appendChild(newElement);
+
+// SE L'USER SCEGLIE DIFFICOLTA 1
+if (choiseDifficult === difficolta[0]) {
+    for (i = 0; i < 100; i++) {
+        let newElement = generaElemento("div", "square");
+        gridCont.appendChild(newElement);
+    } 
+
+    // SE L'USER SCEGLIE DIFFICOLTA 2
+} else if (choiseDifficult === difficolta[1]) {
+    for (i = 0; i < 81; i++) {
+        let newElement = generaElemento("div", "square1");
+        gridCont.appendChild(newElement);
+    }
+
+    // SE L'USER SCEGLIE DIFFICOLTA 3
+} if (choiseDifficult === difficolta[2]) {
+    for (i = 0; i < 49; i++) {
+        let newElement = generaElemento("div", "square2");
+        gridCont.appendChild(newElement);
+    }
 }
+
+
+
 
 
 
