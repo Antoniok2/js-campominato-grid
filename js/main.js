5feab84c44eff451f3fdcf77523134700ee2cada
Antoniok2 <antoniopiazza18@gmail.com>
2021-11-02 17:15:22 +0100

    refactoring code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 let choiseDifficult = prompt("Benvenuto al gioco Campo minato: scegli difficolta: 1, 2, 3");
 const difficolta = ["1", "2", "3"];
 const gridCont = document.querySelector(".grid");
-const squareCont = document.querySelector(".square", "square1", "square2");
 
 
 // SE L'USER SCEGLIE DIFFICOLTA 1
@@ -9,36 +8,29 @@ if (choiseDifficult === difficolta[0]) {
     numeroBlocchi = 100;
     defaultClass = "square";
 
-    // SE L'USER SCEGLIE DIFFICOLTA 2
+// SE L'USER SCEGLIE DIFFICOLTA 2
 } else if (choiseDifficult === difficolta[1]) {
     numeroBlocchi = 81;
     defaultClass = "square1";
 
-    // SE L'USER SCEGLIE DIFFICOLTA 3
+// SE L'USER SCEGLIE DIFFICOLTA 3
 } if (choiseDifficult === difficolta[2]) {
     numeroBlocchi = 49;
     defaultClass = "square2";
 }
 
-
+// CICLO DOPO LA SCELTA DELLA DIFFICOLTA
 for (i = 0; i < numeroBlocchi; i++) {
     let newElement = generaElemento("div", defaultClass);
+    newElement.addEventListener('click', 
+        function() {
+        this.classList.add("selection_square");
+        }
+    );
     gridCont.appendChild(newElement);
 }
 
-squareCont.addEventListener('click',
-    function(){
-        this.classList.add("selection_square")
-    }
-
-
-)
-
-
-
-
-
-
+// FUNZIONI
 function generaElemento(elementoGen, classPlus) {
     let cont = document.createElement(elementoGen);
     cont.classList.add(classPlus);
